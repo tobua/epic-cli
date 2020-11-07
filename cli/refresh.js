74edb2b1d5de9dc4bfcfd2099c2dd807efc8343c
Matthias Giger <i@matthiasgiger.com>
2020-11-07 13:25:30 +0100

    fix(update): use sync method

--- cli/refresh.js
+++ cli/refresh.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import rimraf from 'rimraf'
 import { execSync } from 'child_process'
 
-rimraf(join(process.cwd(), 'node_modules'))
-rimraf(join(process.cwd(), 'package-lock.json'))
+rimraf.sync(join(process.cwd(), 'node_modules'))
+rimraf.sync(join(process.cwd(), 'package-lock.json'))
 
 execSync('npm install', { stdio: 'inherit' })
