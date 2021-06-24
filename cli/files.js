3ae447b6daa865461ecc1c8b994fb42820e7f1cb
Matthias Giger <i@matthiasgiger.com>
2021-06-24 20:39:00 +0200

    feat(scripts): add files script to list nested file structure

---
+++ cli/files.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import tree from 'tree-node-cli'
+
+const files = tree(process.cwd(), {
+  allFiles: true,
+  exclude: [/node_modules/, /dist/],
+  maxDepth: 2,
+})
+
+console.log(files)
