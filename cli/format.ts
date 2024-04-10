18a02ea04b50be1c389e794e1769579b7a500690
Matthias Giger <i@matthiasgiger.com>
2024-04-10 23:11:13 +0200

    fix(bunx): script file path resolution
    Pass on arguments for bunx and migrate pakag changes. release-npm

--- cli/format.ts
+++ cli/format.ts
@@ -1,13 +1,10 @@
 #!/usr/bin/env bun
 import { readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
-import formatPackageJson from 'pakag'
+import { formatPackageJson } from 'pakag'
 
 const packageJsonPath = join(process.cwd(), 'package.json')
-
 const packageContents = readFileSync(packageJsonPath, 'utf8')
-
-// @ts-ignore
-const formattedContents = formatPackageJson(packageContents)
+const formattedContents = await formatPackageJson(packageContents)
 
 writeFileSync(packageJsonPath, formattedContents)
--- cli/standalone.ts
+++ cli/standalone.ts
@@ -5,14 +5,17 @@ import { join } from 'node:path'
 
 // Entry file used for when running through "bunx".
 
-console.log(process.argv, process.cwd(), process.env.PWD, import.meta.url)
-
+const url = new URL(import.meta.url) // Points to this file on the file system.
+const cliFolderPath = url.pathname.split('/').slice(0, -1).join('/')
 const script = process.argv.slice(2)[0]
+const scriptFilePath = join(cliFolderPath, `${script}.ts`)
 
-if (!existsSync(join(process.cwd(), `cli/${script}.ts`))) {
+if (!existsSync(scriptFilePath)) {
   console.error(`Script ${script} not found.`)
 }
 
-execSync(`bun ./cli/${script}.ts`, {
+const additionalArguments = process.argv.slice(3).join(' ')
+
+execSync(`bun ${scriptFilePath} ${additionalArguments}`, {
   stdio: 'inherit',
 })
