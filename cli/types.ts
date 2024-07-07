25587249558bfb5b2b9f5a7ea8d9407a929cc634
Matthias Giger <i@matthiasgiger.com>
2024-07-07 19:25:10 +0200

    fix(types): display error messages
    release-npm

--- cli/types.ts
+++ cli/types.ts
@@ -1,15 +1,37 @@
 #!/usr/bin/env bun
-import { execSync } from 'node:child_process'
+import { type ExecException, execSync } from 'node:child_process'
 
 const listFiles = process.argv.includes('--files')
 
-const tscOutput = execSync('bun tsc --noEmit --listFiles', { encoding: 'utf-8', cwd: process.cwd() })
+let tscOutput: string | undefined
+let hasErrors = false
+
+try {
+  tscOutput = execSync('bun tsc --noEmit --listFiles', { encoding: 'utf-8', cwd: process.cwd(), stdio: 'pipe' })
+} catch (error) {
+  hasErrors = true
+  tscOutput = (error as ExecException).stdout
+}
+
+if (!tscOutput) {
+  console.log('Failed to generate output.')
+  process.exit(0)
+}
 
 const processedOutput = tscOutput
   .split('\n')
   .filter((line) => !line.includes('node_modules'))
   .map((line) => line.replace(process.cwd(), '.'))
   .filter((line) => line.trim() !== '')
+
+if (hasErrors) {
+  // TODO ascii formatting of output isn't preserved currently, ascii encoding will not solve the issue.
+  const filteredOutput = tscOutput
+    .split('\n')
+    .filter((line) => !line.startsWith(process.cwd()))
+    .join('\n')
+  console.log(filteredOutput)
+}
 
 const filePaths = processedOutput.map((line) => line.split(':')[0])
 const uniqueFilePaths = [...new Set(filePaths)]
--- test/types.test.ts
+++ test/types.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from 'bun:test'
 import { execSync } from 'node:child_process'
+import { rmSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
 
 test('Checks types and lists file count.', () => {
   const output = execSync('bun ./cli/types.ts', {
@@ -17,4 +19,25 @@ test('Checks types with --files flag and lists specific files.', () => {
 
   expect(output).toContain('helper.ts')
   expect(output).toContain('cli/files.ts')
+})
+
+test('Displays error messages.', () => {
+  const filePath = join(process.cwd(), 'cli/has-type-errors.ts')
+
+  writeFileSync(
+    filePath,
+    `const what: number = 'what?'
+const another: string = 6
+console.log(what, another)`,
+  )
+  const output = execSync('bun ./cli/types.ts --files', {
+    cwd: '.',
+  }).toString()
+
+  expect(output).toContain("Type 'string' is not")
+  expect(output).toContain("Type 'number' is not")
+  expect(output).not.toContain(process.cwd())
+  expect(output).toContain('cli/has-type-errors.ts')
+
+  rmSync(filePath)
 })
